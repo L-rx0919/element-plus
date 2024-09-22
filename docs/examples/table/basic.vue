584937f413412a7df2ea84c104ff28ae1b644aec
L-rx0919 <x13178463260@outlook.com>
2024-09-22 20:20:56 +0800

    Update basic.vue

--- docs/examples/table/basic.vue
+++ docs/examples/table/basic.vue
@@ -1,4 +1,17 @@
 <template>
+  <el-row>
+        <el-col :span="5">
+            <el-text>商品名称：</el-text>
+            <el-input v-model="search.GoodsName" style="width: 170px" placeholder="请输入商品名称" clearable />
+        </el-col>
+        <el-col :span="6">
+            <el-text>创建日期：</el-text>
+            <el-date-picker v-model="search.CreateTime" type="date" placeholder="选择创建日期" value-format="YYYY-MM-DD" />
+        </el-col>
+        <el-col :span="3">
+            <el-button type="primary" @click="handleGetGoodsList">查询</el-button>
+        </el-col>
+    </el-row>
   <el-table :data="tableData" style="width: 100%">
     <el-table-column prop="date" label="Date" width="180" />
     <el-table-column prop="name" label="Name" width="180" />
